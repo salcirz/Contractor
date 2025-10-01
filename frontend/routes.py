249378a842ab00fc5027b02a7f7139ad9ed7f4ef
Salvatore Cirisano <salvatorecirisano@mail.adelphi.edu>
2025-10-01 12:30:23 -0400

    redo routes

--- frontend/routes.py
+++ frontend/routes.py
@@ -1,3 +1,49 @@
-from flask import Flask
+from flask import Flask,request
+
+import mysql.connector
+
+
+app = Flask(_name_)
+def getdb ():
+
+    conn = mysql.connector.connect(
+
+        hostname = "compsci.adelphi.edu",
+        username = "salvatorecirisano",
+        password = "",
+        database = "salvatorecirisano"
+
+    )
+
+    return conn
+
+
+@app.route("/search", methods= ["POST"])
+def search():
+    currentjob = request.form["jobsearch"] 
+
+    querey = "Select * from picture where jobtype = %s"
+
+    db = getdb()
+
+    cursor = db.cursor()
+
+    cursor.execute(querey, (currentjob,))
+
+    results = cursor.fetchall()
+
+    db.close()
+
+    html = ""
+    for row in results:
+        html += f"<p>{row}</p>"
+
+    return html
+
+
+
+
+
+
 
 
