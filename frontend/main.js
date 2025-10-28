30b607699cd6ad96e6a90f861d6ada7bda6ce9a8
Salvatore Cirisano <salvatorecirisano@mail.adelphi.edu>
2025-10-28 16:41:25 -0400

    edit main

---
+++ frontend/main.js
@@ -0,0 +1,13 @@
+console.log(1);
+document.getElementById("file").addEventListener("change", (event) => {
+
+    const file = event.target.files[0];
+    const image = URL.createObjectURL(file);
+
+    const center =  document.getElementById("main");
+    center.style.backgroundImage = `url(${image})`;
+    center.style.backgroundSize = "cover";
+    center.style.backgroundPosition = "center";
+
+
+});
