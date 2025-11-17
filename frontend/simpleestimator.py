d70293f9224b9748558d413ddfcc1d584a29e2e6
Salvatore Cirisano <salvatorecirisano@mail.adelphi.edu>
2025-11-17 13:08:18 -0500

    frontend edits

--- frontend/simpleestimator.py
+++ frontend/simpleestimator.py
@@ -20,6 +20,13 @@ import os
 
 load_dotenv()
 
+
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_REGION = os.getenv("AWS_REGION")
+S3_BUCKET = os.getenv("S3_BUCKET")
+
 app = Flask(__name__)
 app.secret_key = "secretttkeke"
 
